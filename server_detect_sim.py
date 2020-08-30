2fb8998608d0132db8abfe458027d6d1ffdcad7f
xbjin <sonpking@163.com>
2020-08-30 16:39:58 +0800

    run successfully

--- server_detect_sim.py
+++ server_detect_sim.py
@@ -1,21 +1,22 @@
-from detection import SelectiveSearch, Camera, generate_boxes, generate_bb, show_bb, vis_bb, Resize, filter_rects
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 17:15:31 2020
+@author: Paul King
+"""
+from detection import Camera, generate_boxes, generate_bb, show_bb, vis_bb, Resize, filter_rects, SelectiveSearch
 from detection.generate_img_from_video import generate_img
-import cv2 as cv
 from models.BananaNet import banananetv2_spike
 from models.MobileNet_Slim import mobilenet_slim_spike
 from models import SpikeNet
 from util import load_pretrained
 from util.validate import eval_single
+from demo.detect_client import client_run
+from demo.detect_server import server_run, test_server_run
+from multiprocessing import Process
 import torch
 import numpy as np
 import pickle
 import cv2 as cv
-
-
-from demo.detect_client import client_run
-from demo.detect_server import server_run, test_server_run
-from multiprocessing import Process
-
 
 if __name__ == "__main__":
     # server_run("localhost", 10080)
